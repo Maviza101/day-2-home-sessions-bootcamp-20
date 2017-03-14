4c25f73f9a2ec420a53000f95dcb9999b66ba2b2
Folusho <folusho@lanthanidesoft.com>
2017-03-14 16:46:05 +0100

    update test cases

--- tests/displayItemsSpec.js
+++ tests/displayItemsSpec.js
@@ -3,36 +3,37 @@
 describe("Unit Tests for displayItems()", function() {
 	var myApp = require('../app/displayItems.js');
 	var displayItems = myApp.displayItems;
+	var itemsKey = 'artissts';
 	describe("Test that it accepts only valid arguments", function() {
-		it("should return `Argument is not an object` for []", function() {
-			expect(displayItems([])).toEqual("Argument is not an object");
+		it("should return `First argument must not be an array` for []", function() {
+			expect(displayItems([], itemsKey)).toEqual("First argument must not be an array");
 		});
 		
-		it("should return `Argument is not an object` for 54", function() {
-			expect(displayItems([])).toEqual("Argument is not an object");
+		it("should return `First argument is not an object` for 54", function() {
+			expect(displayItems(54, itemsKey)).toEqual("First argument is not an object");
 		});
 		
-		it("should return `Argument is not an object` for a function", function() {
+		it("should return `First argument is not an object` for a function", function() {
 			var cheekPosition = function() {
 				return 'On the face';
 			}
-			expect(displayItems(cheekPosition)).toEqual("Argument is not an object");
+			expect(displayItems(cheekPosition, itemsKey)).toEqual("First argument is not an object");
 		});
 		
-		it("should return `Argument is not an object` for false", function() {
-			expect(displayItems(false)).toEqual("Argument is not an object");
+		it("should return `First argument is not an object` for false", function() {
+			expect(displayItems(false, itemsKey)).toEqual("First argument is not an object");
 		});
 		
-		it("should return `Argument is not an object` for undefined", function() {
-			expect(displayItems(undefined)).toEqual("Argument is not an object");
+		it("should return `First argument is not an object` for undefined", function() {
+			expect(displayItems(undefined, itemsKey)).toEqual("First argument is not an object");
 		});
 		
-		it("should return `Argument must not be null` for null", function() {
-			expect(displayItems(null)).toEqual("Argument must not be null");
+		it("should return `First argument must not be null` for null", function() {
+			expect(displayItems(null, itemsKey)).toEqual("First argument must not be null");
 		});
 		
-		it("should return `Object argument must not be empty` for {}", function() {
-			expect(displayItems({})).toEqual("Object argument must not be empty");
+		it("should return `First argument must not be an empty object` for {}", function() {
+			expect(displayItems({}, itemsKey)).toEqual("First argument must not be an empty object");
 		});
 	});
 });
