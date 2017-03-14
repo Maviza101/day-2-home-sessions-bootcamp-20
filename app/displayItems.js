116af58610e286b567a2150282a0470c81067b9a
Folusho <folusho@lanthanidesoft.com>
2017-03-14 19:11:33 +0100

    write code to pass all tests

--- app/displayItems.js
+++ app/displayItems.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports.displayItems = function displayItems(obj, itemsKey) {
+module.exports.displayItems = function(obj, itemsKey) {
   // Validate input.
 	if (typeof obj != 'object') {
     return 'First argument is not an object';
@@ -11,32 +11,30 @@ module.exports.displayItems = function displayItems(obj, itemsKey) {
   if (obj instanceof Array) {
     return 'First argument must not be an array';
   }
-  if (Object.keys(obj).length  && obj.constructor == Object) {
+  if (Object.keys(obj).length == 0) {
     return 'First argument must not be an empty object';
-  };
-
-  var statusMessge = '';
-  try {    
-    var items = obj[itemsKey];
-
-    for (var i = 0; i < items.length; i++) {
-      var item = items[i];
-      // Responses for albums, playlists and tracks don't have followers and popularity.
-      var itemDetails = 'Name: ' + item.name + '\n' +
-            'Type: ' + item.type + '\n' + 
-            'Followers: ' + item.followers.total + '\n' + 
-            'Popularity: ' + item.popularity + '\n' +
-            'Spotify URL: ' + item.external_urls.spotify + '\n' + 
-            '\n';
-      console.log(itemDetails);
-    }
-
   }
-  catch (e) {
-    if (e.message == 'Cannot read property \'length\' of undefined') {
-      statusMessge = 'First argument must not be an empty object';      
-    }
+  if (itemsKey == undefined) {
+    return 'Second argument must be defined';
+  }
+  if (typeof itemsKey != 'string') {
+    return 'Second argument must be a string';
   }
 
-  return statusMessge;
+  var statusMessge = 'ff';
+  var output = '';
+  var items = obj[itemsKey].items;
+  for (var i = 0; i < items.length; i++) {
+    var item = items[i];
+    // Responses for albums, playlists and tracks don't have followers and popularity.
+    var itemDetails = 'Name: ' + item.name + '\n' +
+          'Type: ' + item.type + '\n' + 
+          'Followers: ' + item.followers.total + '\n' + 
+          'Popularity: ' + item.popularity + '\n' +
+          'Spotify URL: ' + item.external_urls.spotify + '\n' + 
+          '\n';
+    output += itemDetails;
+  }
+
+  return output;
 }
