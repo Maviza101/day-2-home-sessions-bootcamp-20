58d8c126efcdaec45cea0cd68be1a1fb47487bf0
Folusho <folusho@lanthanidesoft.com>
2017-03-14 16:59:41 +0100

    add test cases

--- tests/displayItemsSpec.js
+++ tests/displayItemsSpec.js
@@ -3,7 +3,8 @@
 describe("Unit Tests for displayItems()", function() {
 	var myApp = require('../app/displayItems.js');
 	var displayItems = myApp.displayItems;
-	var itemsKey = 'artissts';
+	var itemsKey = 'artists';
+	var results = {items: "foo"};
 	describe("Test that it accepts only valid arguments", function() {
 		it("should return `First argument must not be an array` for []", function() {
 			expect(displayItems([], itemsKey)).toEqual("First argument must not be an array");
@@ -34,6 +35,22 @@ describe("Unit Tests for displayItems()", function() {
 		
 		it("should return `First argument must not be an empty object` for {}", function() {
 			expect(displayItems({}, itemsKey)).toEqual("First argument must not be an empty object");
+		});
+		
+		it("should return `Second argument must be a string` for 90", function() {
+			expect(displayItems(items, 90)).toEqual("Second argument must be a string");
+		});
+		
+		it("should return `Second argument must be a string` for {}", function() {
+			expect(displayItems(items, {})).toEqual("Second argument must be a string");
+		});
+		
+		it("should return `Second argument must be a string` for false", function() {
+			expect(displayItems(items, false)).toEqual("Second argument must be a string");
+		});
+		
+		it("should return `Second argument must be defined` if it is omitted", function() {
+			expect(displayItems(items)).toEqual("Second argument must be defined");
 		});
 	});
 });
